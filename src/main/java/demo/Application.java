84e16c12f5390abf2d8fb1273e2e95e09660d9d6
Philipp-p <Philipp-p@users.noreply.github.com>
2023-04-26 15:29:56 +0200

    added second transaction and modified IFC file

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -17,15 +17,19 @@ import utils.RepositoryUtils;
 public class Application {
   public static final String RDF_4_J_SERVER = "http://localhost:8080/rdf4j-server";
 
-  public static final String IFC_FILE = "BasicWall.ifc";
+  public static final String IFC_FILE = "Week_37_11_sept_IFC_Schependomlaan_incl_planningsdata.ifc";
+  public static final String IFC_FILE_UPDATED =
+      "Week_37_11_sept_IFC_Schependomlaan_incl_planningsdata2.ifc";
   public static final String IFC_PATH = "./IFC/" + IFC_FILE;
+  public static final String IFC_PATH_UPDATED = "./IFC/" + IFC_FILE_UPDATED;
   public static final String REPOSITORY_ID = "demo_ifc";
 
   public static void main(String[] args) {
     RepositoryConnection connection = null;
 
     try {
-      logMessage("Loading IFC model...");
+      // ifc model
+      logMessage("Loading IFC model");
       String ifcModel = Files.readString(Path.of(IFC_PATH), Charset.defaultCharset());
       if (ifcModel == null) {
         logMessage("ifcModel is null");
@@ -58,10 +62,37 @@ public class Application {
       connection.commit();
       logMessage("First transaction committed");
 
+      // updated ifc model
+      logMessage("Loading updated ifc model");
+      String ifcModelUpdated =
+          Files.readString(Path.of(IFC_PATH_UPDATED), Charset.defaultCharset());
+      if (ifcModelUpdated == null) {
+        logMessage("ifcModelUpdated is null");
+        return;
+      }
+
+      logMessage("Converting updated ifc model to rdf");
+      String rdfModelUpdated = converter.toRdf(ifcModelUpdated);
+
+      logMessage("Writing updated named graph");
+      connection.begin();
+
+      // delete context
+      connection.clear(context);
+
+      inputStream = new ByteArrayInputStream(rdfModelUpdated.getBytes());
+      connection.add(inputStream, RDFFormat.TURTLE, context);
+
+      logMessage("Committing second transaction");
+      connection.commit();
+      logMessage("Second transaction committed");
+
+      connection.close();
+
     } catch (Exception e) {
       e.printStackTrace();
-      if (connection != null && connection.isActive()) {
-        connection.rollback();
+      if (connection != null) {
+        connection.close();
       }
       throw new RuntimeException(e);
     }
